a473b8160dd8a95545114bd2f01fd7e7da0b2846
waynelee-lwc <1418551940@qq.com>
2023-03-17 11:49:16 +0800

    some update

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+CONTAINER_NAME=cowstudio/be-sample
+
 bash build.sh
 
+# 关闭容器
+echo "removing....."
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+
 # 运行容器
-docker run -p 3000:8080 -d cowstudio/be-sample
+echo "starting....."
+docker run -p 3000:8080 -d $CONTAINER_NAME
