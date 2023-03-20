36df0782b39c7be79844071195e010b035b2d174
waynelee-lwc <1418551940@qq.com>
2023-03-21 00:06:15 +0800

    update shell

--- build/run.sh
+++ build/run.sh
@@ -5,12 +5,12 @@ go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 
 # 构建应用
-go build -o app main.go
+go build -o /app/app main.go
 
 # 安装sidecar
 export PATH=$PATH:`go env GOPATH`/bin
 go install github.com/jlu-cow-studio/sidecar-service@latest
 
-app
+/app/app
 
 sidecar-service $ENV_SERVICE_NAME $ENV_SERVICE_ADDRESS $ENV_SERVICE_PORT $ENV_SIDECAR_PORT
